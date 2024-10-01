89c8960200126becde98bf6ce1abd82a57b92816
alter-at-sps <soucekd23@spstrutnov.cz>
2024-10-01 11:16:36 +0200

    finish pis

--- C-K-P/pis/Program.cs
+++ C-K-P/pis/Program.cs
@@ -2,21 +2,23 @@
 {
     internal class Program
     {
-        static int[,] map = new int[3,3];
+        static int[,] map;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, piskvork!");
             Random rand = new Random();
 
+            ResetMap();
+
             while (true)
             {
                 DrawMap();
 
                 Console.Write("\n Place x: ");
-                string p_x = Console.ReadLine();
+                string? p_x = Console.ReadLine();
                 Console.Write(" Place y: ");
-                string p_y = Console.ReadLine();
+                string? p_y = Console.ReadLine();
 
                 if (p_x == null || p_y == null) return;
                 if (p_x == "" || p_y == "") return;
@@ -30,87 +32,138 @@
                     continue;
                 }
 
-                map[player_x, player_y] = 1;
+                map[player_y, player_x] = 1;
 
                 int cpu_x = -1, cpu_y = -1;
 
                 while (!CheckValid(cpu_x, cpu_y))
                 {
-                    cpu_x = rand.Next(0, 3);
-                    cpu_y = rand.Next(0, 3);
+                    cpu_x = rand.Next(0, map.GetLength(1));
+                    cpu_y = rand.Next(0, map.GetLength(0));
                 }
 
                 Console.WriteLine($" Cpu x: {cpu_x}");
-                Console.WriteLine($" Cpu x: {cpu_y}");
+                Console.WriteLine($" Cpu y: {cpu_y}");
 
-                map[cpu_x, cpu_y] = 2;
+                map[cpu_y, cpu_x] = 2;
+
+                int finish_status = CheckIfDone();
+
+                switch (finish_status)
+                {
+                    case 0:
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Player wins!");
+                        ResetMap();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("CPU wins!");
+                        ResetMap();
+                        break;
+                }
             }
+        }
+
+        static void ResetMap()
+        {
+            Console.Write("\n Map size: ");
+            string? s = Console.ReadLine();
+
+            if (s == null) return;
+            if (s == "") return;
+
+            int map_size = Int32.Parse(s);
+
+            map = new int[map_size, map_size]; // reset
         }
 
         static void DrawMap()
         {
-            Console.WriteLine($"\n|---|---|---|");
+            Console.Write($"\n");
+            for (int j = 0; j < map.GetLength(1); j++)
+                Console.Write($"|---");
+            Console.WriteLine($"|");
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                Console.WriteLine($"| {map[i, 0]} | {map[i, 1]} | {map[i, 2]} |");
-                Console.WriteLine($"|---|---|---|");
+                for (int j = 0; j < map.GetLength(1); j++) 
+                    Console.Write($"| {map[i, j]} ");
+                Console.WriteLine($"|");
+
+                for (int j = 0; j < map.GetLength(1); j++)
+                    Console.Write($"|---");
+                Console.WriteLine($"|");
             }
         }
 
         static bool CheckValid(int x, int y)
         {
-            if (x < 0 || x > 2) return false;
-            if (y < 0 || y > 2) return false;
+            if (x < 0 || x >= map.GetLength(1)) return false;
+            if (y < 0 || y >= map.GetLength(0)) return false;
 
-            return map[x, y] == 0;
+            return map[y, x] == 0;
         }
 
-        static bool CheckIfDone()
+        static int CheckIfDone()
         {
             // rows
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < map.GetLength(1); i++)
             {
-                bool all = true;
+                int match = map[i,0];
 
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < map.GetLength(0); j++)
                 {
-                    if (map[i,j] == 0) all = false;
+                    if (map[i,j] != match)
+                    {
+                        match = -1;
+                        break;
+                    }
                 }
 
-                if (all) return true;
+                if (match != -1 && match != 0) return match;
             }
 
             // collums
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < map.GetLength(1); i++)
             {
-                bool all = true;
+                int match = map[0, i];
 
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < map.GetLength(0); j++)
                 {
-                    if (map[j, i] == 0) all = false;
+                    if (map[i, j] != match)
+                    {
+                        match = -1;
+                        break;
+                    }
                 }
 
-                if (all) return true;
+                if (match != -1 && match != 0) return match;
             }
 
             // diags
 
             for (int d = 0; d < 2; d++)
             {
-                bool all = true;
+                int match = map[0, d * (map.GetLength(1) - 1)];
 
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < map.GetLength(0); i++)
                 {
-                    if (map[i, (2 * d) + (i * -d)] == 0) all = false;
+                    if (map[i, ((map.GetLength(1) - 1) * d) + (i * -d)] != match)
+                    {
+                        match = -1;
+                        break;
+                    };
                 }
 
-                if (all) return true;
+                if (match != -1 && match != 0) return match;
             }
 
-            return false;
+            return 0;
         }
     }
 }
